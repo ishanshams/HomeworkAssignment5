2cdda8e0b1279cff73f701c332e457a1e3b27f33
ishanshams <ishanshams303@gmail.com>
2015-11-17 13:32:29 -0500

    2nd push
    binaryTreeApp posted

---
+++ src/BinaryTreeApp.java
@@ -0,0 +1,37 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ishan
+ */
+public class BinaryTreeApp {
+    public static void main(String[] args) {
+        BTNode<Integer> tree = new BTNode<Integer>(null, null, null);
+		
+			if(tree.getData()==null) tree.setData(47);
+                        
+			tree.addNode(new BTNode<Integer>(25, null, null));
+                        tree.addNode(new BTNode<Integer>(77, null, null));
+                        tree.addNode(new BTNode<Integer>(11, null, null));
+                        tree.addNode(new BTNode<Integer>(43, null, null));
+                        tree.addNode(new BTNode<Integer>(65, null, null));
+                        tree.addNode(new BTNode<Integer>(93, null, null));
+                        tree.addNode(new BTNode<Integer>(7, null, null));
+                        tree.addNode(new BTNode<Integer>(17, null, null));
+                        tree.addNode(new BTNode<Integer>(31, null, null));
+                        tree.addNode(new BTNode<Integer>(44, null, null));
+                        tree.addNode(new BTNode<Integer>(68, null, null));
+                        
+		
+		tree.print(tree.getHeight());
+                System.out.println("\n\n"+ tree.getRoot().getData() + " is a root node");
+                System.out.println("\n\n"+ tree.getRoot().getData() + " is a root node");
+    }
+}
